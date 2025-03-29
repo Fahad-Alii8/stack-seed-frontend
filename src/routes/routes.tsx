c9e0b70288810826f414a61d2dd9aea78fd81730
Fahad-Alii8 <mfahadali.sparkingasia@gmail.com>
2025-03-29 21:06:12 +0500

    Setup routing

---
+++ src/routes/routes.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+
+// createBrowserRouter is a function provided by react-router-dom that defines an array of route objects.  
+// Each object corresponds to a specific route and its associated component.  
+// It helps manage application routing efficiently without requiring a full page reload.  
+// It leverages the browser’s built-in navigation system (window.history.pushState) to enable seamless navigation between pages.  
+
+
+const router = createBrowserRouter([
+
+]);
+
+export default router
